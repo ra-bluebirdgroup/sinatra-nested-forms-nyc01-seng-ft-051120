70290bc3f3ccf74ba5cdd0c81786b621d1e2014e
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-06-03 19:10:07 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    Pirate.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
